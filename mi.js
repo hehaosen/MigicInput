d4c850ea906546e386c0a5f559869f10eae5e649
hehaosen <307052084@qq.com>
2016-08-04 09:04:02 +0800

    dd

--- mi.js
+++ mi.js
@@ -1,3 +1,12 @@
-var mi = function (element) {
+var mi = function (element, nature) {
+    var ele = document.querySelector(element);
+    
 
+    // 潜复制
+    function exend (target, options) {
+        for (name in options) {
+            target[name] = options[name];
+        }
+        return target;
+    }
 };
