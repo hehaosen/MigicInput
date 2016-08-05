a7f7308223fc5cd78646dcf00bb4e2bcc5744f85
hehaosen <307052084@qq.com>
2016-08-05 09:13:06 +0800

    增加input判断

--- mi.js
+++ mi.js
@@ -1,6 +1,12 @@
 var mi = function (element, nature) {
     var ele = document.querySelector(element);
-    
+
+    if (ele.localName !== 'input') {
+        console.warn('节点' + element + ',不是一个输入框');
+        return;
+    }
+
+    //ele.readonly = 'readonly';
 
     // 潜复制
     function exend (target, options) {
