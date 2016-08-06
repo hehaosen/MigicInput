acb4d3f20935a8d778441d819edc4d911b9f6559
hehaosen <307052084@qq.com>
2016-08-06 13:01:50 +0800

    dd

--- mi.js
+++ mi.js
@@ -1,12 +1,17 @@
 var mi = function (element, nature) {
     var ele = document.querySelector(element);
-
     if (ele.localName !== 'input') {
         console.warn('节点' + element + ',不是一个输入框');
         return;
     }
 
-    //ele.readonly = 'readonly';
+    ele.setAttribute('readonly', 'readonly');
+
+    ele.addEventListener('click', function () {
+
+    });
+
+    // 点击
 
     // 潜复制
     function exend (target, options) {
