2ee474a095faae74b23211ae3673ab27c1f69fb7
hehaosen <307052084@qq.com>
2016-08-08 09:44:06 +0800

    dd

--- mi.js
+++ mi.js
@@ -5,16 +5,42 @@ var mi = function (element, nature) {
         return;
     }
 
+    init();
+
     // 将原先节点禁止写入
     ele.setAttribute('readonly', 'readonly');
 
+
     ele.addEventListener('click', function () {
-        console.log('aaa');
+
     });
 
-    var newEle = document.createElement("div");
-    newEle.id = 'J_MIBG';
-    document.body.appendChild(newEle);
+    // 初始化
+    function init () {
+        var newEle = document.createElement("div");
+        newEle.id = 'J_MIBG';
+        document.body.appendChild(newEle);
+    }
+
+    // 样式方法
+    function styles (element, css) {
+
+        if (typeof (css) !== 'object') {
+            console.warn('function styles 报错 传入css参数不是一个对象');
+            return false;
+        }
+
+        var cssText = '';
+
+        // 合并对象中的css样式
+        for (var i in css) {
+            cssText += i + ':' + css[i] + ';'
+        }
+
+        element.cssText = cssText;
+
+        return true
+    }
 
     // 潜复制
     function exend (target, options) {
