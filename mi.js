3cd178344daebca24f25ecf873bc5adcbd4a098d
hehaosen <307052084@qq.com>
2016-08-09 10:19:10 +0800

    dd

--- mi.js
+++ mi.js
@@ -20,16 +20,17 @@ var mi = function (element, nature) {
         var newEle = document.createElement("div");
         newEle.id = 'J_MIBG';
         document.body.appendChild(newEle);
+        styles(document.querySelector('#J_MIBG'), {
+            'height' : '100px'
+        })
     }
 
     // 样式方法
     function styles (element, css) {
-
         if (typeof (css) !== 'object') {
             console.warn('function styles 报错 传入css参数不是一个对象');
             return false;
         }
-
         var cssText = '';
 
         // 合并对象中的css样式
@@ -37,7 +38,7 @@ var mi = function (element, nature) {
             cssText += i + ':' + css[i] + ';'
         }
 
-        element.cssText = cssText;
+        element.style.cssText = cssText;
 
         return true
     }
